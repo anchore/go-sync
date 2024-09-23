8edfdd6d6749c25a30a6ef2dc3318eda3b2a0c3d
Alex Goodman <wagoodman@users.noreply.github.com>
2024-09-23 16:10:39 -0400

    decouple collector and executor
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- collector.go
+++ collector.go
@@ -27,10 +27,13 @@ type collector[T any] struct {
 	executor Executor
 	out      []T
 	mu       sync.Mutex
+	wg       sync.WaitGroup
 }
 
 func (c *collector[T]) Provide(provider ProviderFunc[T]) {
+	c.wg.Add(1)
 	c.executor.Execute(func() {
+		defer c.wg.Done()
 		values := provider()
 		c.mu.Lock()
 		c.out = append(c.out, values)
@@ -39,7 +42,7 @@ func (c *collector[T]) Provide(provider ProviderFunc[T]) {
 }
 
 func (c *collector[T]) Collect() (everything []T) {
-	c.executor.Wait()
+	c.wg.Wait()
 	c.mu.Lock()
 	everything = c.out
 	c.out = nil

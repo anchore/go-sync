abcbe3208053cac1e783950eb24a7c29f7ea3430
Keith Zantow <kzantow@gmail.com>
2025-02-15 14:36:56 -0500

    chore: update tests
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- context_test.go
+++ context_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestContextExecutor(t *testing.T) {
+func Test_GetExecutor(t *testing.T) {
 	t.Run("WithExecutorInContext", func(t *testing.T) {
 		executor := NewExecutor(2)
 		ctx := SetContextExecutor(context.Background(), "cpu", executor)
 
-		result := ContextExecutor(ctx, "cpu")
+		result := GetExecutor(ctx, "cpu")
 
 		require.NotNil(t, result)
 		require.IsType(t, &boundedExecutor{}, result)
@@ -21,7 +21,7 @@ func TestContextExecutor(t *testing.T) {
 	t.Run("WithoutExecutorInContext", func(t *testing.T) {
 		ctx := context.Background()
 
-		result := ContextExecutor(ctx, "cpu")
+		result := GetExecutor(ctx, "cpu")
 
 		require.NotNil(t, result)
 		require.IsType(t, sequentialExecutor{}, result)
@@ -31,7 +31,7 @@ func TestContextExecutor(t *testing.T) {
 		executor := NewExecutor(1)
 		ctx := SetContextExecutor(context.Background(), "cpu", executor)
 
-		result := ContextExecutor(ctx, "io")
+		result := GetExecutor(ctx, "io")
 
 		require.NotNil(t, result)
 		require.IsType(t, sequentialExecutor{}, result)
